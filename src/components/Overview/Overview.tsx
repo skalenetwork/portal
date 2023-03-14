992664f3d691cca7d70d13dad4f8438eb07d2caa
Dmytro <dmitry.tkk@gmail.com>
2023-03-14 19:19:45 +0000

    Trigger staging build

--- src/components/Overview/Overview.tsx
+++ src/components/Overview/Overview.tsx
@@ -4,7 +4,6 @@ import Stack from '@mui/material/Stack';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 
-
 import { ICONS, CHAINS } from '../../core/constants';
 import ChainOverview from '../ChainOverview';
 
