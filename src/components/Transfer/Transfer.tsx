2b93498ca5262c6a8829e436f4b972ec70513405
Dmytro <dmitry.tkk@gmail.com>
2023-03-14 19:32:30 +0000

    Fix transfer complete event

--- src/components/Transfer/Transfer.tsx
+++ src/components/Transfer/Transfer.tsx
@@ -99,11 +99,6 @@ export default function Transfer(props: any) {
             transferComplete,
             false
         );
-        window.addEventListener(
-            "metaport_transferComplete",
-            transferComplete,
-            false
-        );
         setWeb3(initChainWeb3(fromChain));
         setWeb3Dest(initChainWeb3(toChain));
         let balanceUpdateTimer = setInterval(() => setUpdateBalanceFlag(!updateBalanceFlag), 10 * 1000);
