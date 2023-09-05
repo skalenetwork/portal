5739a60126b509ca64f4bac5c67220dab1dbbf1c
Dmytro <dmitry.tkk@gmail.com>
2023-09-05 19:32:40 +0100

    Fix path in index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import mdx from "@mdx-js/rollup"
-import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mdx(), vercel()]
+  plugins: [react(), mdx()]
   // assetsInclude: ["**/*.mdx"]
 })
