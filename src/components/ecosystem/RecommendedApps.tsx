30d140288a6f6a48859da4f348237ef932086fef
Dmytro <dmitry.tkk@gmail.com>
2024-10-25 13:50:34 +0100

    Add recommended apps component, add similar apps funcs

---
+++ src/components/ecosystem/RecommendedApps.tsx
@@ -0,0 +1,100 @@
+/**
+ * @license
+ * SKALE portal
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file RecommendedApps.tsx
+ * @copyright SKALE Labs 2024-Present
+ */
+
+import React, { useMemo } from 'react'
+import { Grid, Box } from '@mui/material'
+import { cls } from '@skalenetwork/metaport'
+
+import { type types } from '@/core'
+
+import { isNewApp, isTrending } from '../../core/ecosystem/utils'
+import { findSimilarApps, type SimilarApp } from '../../core/ecosystem/similarApps'
+
+import AppCardV2 from './AppCardV2'
+import Carousel from '../Carousel'
+import { useLikedApps } from '../../LikedAppsContext'
+
+interface RecommendedAppsProps {
+  skaleNetwork: types.SkaleNetwork
+  chainsMeta: types.ChainsMetadataMap
+  allApps: types.AppWithChainAndName[]
+  currentApp?: types.AppWithChainAndName
+  favoriteApps?: types.AppWithChainAndName[]
+  newApps: types.AppWithChainAndName[]
+  trendingApps: types.AppWithChainAndName[]
+  useCarousel?: boolean
+  className?: string
+}
+
+const RecommendedApps: React.FC<RecommendedAppsProps> = ({
+  skaleNetwork,
+  chainsMeta,
+  allApps,
+  currentApp,
+  favoriteApps,
+  newApps,
+  trendingApps,
+  useCarousel = false,
+  className
+}) => {
+  const { getMostLikedApps, getAppId, getMostLikedRank } = useLikedApps()
+  const mostLikedAppIds = useMemo(() => getMostLikedApps(), [getMostLikedApps])
+
+  const similarApps = useMemo(() => {
+    return findSimilarApps(currentApp, allApps, favoriteApps)
+  }, [currentApp, allApps, favoriteApps])
+
+  if (similarApps.length === 0) return null
+
+  const renderAppCard = (app: SimilarApp) => {
+    const appId = getAppId(app.chain, app.appName)
+    return (
+      <Box key={`${app.chain}-${app.appName}`} className={cls('fl-centered dappCard')}>
+        <AppCardV2
+          skaleNetwork={skaleNetwork}
+          schainName={app.chain}
+          appName={app.appName}
+          chainsMeta={chainsMeta}
+          isNew={isNewApp({ chain: app.chain, app: app.appName }, newApps)}
+          mostLiked={getMostLikedRank(mostLikedAppIds, appId)}
+          trending={isTrending(trendingApps, app.chain, app.appName)}
+          gray={false}
+        />
+      </Box>
+    )
+  }
+  if (useCarousel) {
+    return <Carousel className={className}>{similarApps.map(renderAppCard)}</Carousel>
+  }
+  return (
+    <Grid container spacing={2} className={className}>
+      {similarApps.map((app) => (
+        <Grid key={`${app.chain}-${app.appName}`} item xs={12} sm={6} md={4} lg={4}>
+          {renderAppCard(app)}
+        </Grid>
+      ))}
+    </Grid>
+  )
+}
+
+export default React.memo(RecommendedApps)
---
+++ src/core/ecosystem/similarApps.ts
@@ -0,0 +1,87 @@
+/**
+ * @license
+ * SKALE portal
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file similarApps.tsx
+ * @copyright SKALE Labs 2024-Present
+ */
+
+import { type types } from '@/core'
+import { APP_SUBCATEGORY_MATCH_WEIGHT, MAX_APPS_DEFAULT } from '../constants'
+
+interface CategoryMatch {
+  categoryMatches: number
+  subcategoryMatches: number
+  totalScore: number
+}
+
+interface SimilarApp extends types.AppWithChainAndName {
+  score: number
+}
+
+function calculateCategoryMatches(
+  sourceApp: types.AppWithChainAndName,
+  targetApp: types.AppWithChainAndName
+): CategoryMatch {
+  let categoryMatches = 0
+  let subcategoryMatches = 0
+
+  Object.entries(sourceApp.categories).forEach(([category, sourceSubcategories]) => {
+    if (category in targetApp.categories) {
+      categoryMatches++
+      const targetSubcategories = targetApp.categories[category]
+      if (Array.isArray(sourceSubcategories) && Array.isArray(targetSubcategories)) {
+        const matches = sourceSubcategories.filter((sub) =>
+          targetSubcategories.includes(sub)
+        ).length
+        subcategoryMatches += matches
+      }
+    }
+  })
+
+  const totalScore = categoryMatches + subcategoryMatches * APP_SUBCATEGORY_MATCH_WEIGHT
+  return { categoryMatches, subcategoryMatches, totalScore }
+}
+
+function findSimilarApps(
+  currentApp: types.AppWithChainAndName | undefined,
+  allApps: types.AppWithChainAndName[],
+  favoriteApps: types.AppWithChainAndName[] = [],
+  limit: number = MAX_APPS_DEFAULT
+): SimilarApp[] {
+  const apps = allApps.filter(
+    (app) => !currentApp || app.chain !== currentApp.chain || app.appName !== currentApp.appName
+  )
+
+  const targetApps = currentApp ? [currentApp] : favoriteApps
+  if (!targetApps.length) return []
+
+  const appScores: SimilarApp[] = apps.map((app) => {
+    const scores = targetApps.map((targetApp) => calculateCategoryMatches(targetApp, app))
+
+    const maxScore = Math.max(...scores.map((s) => s.totalScore))
+    return { ...app, score: maxScore }
+  })
+
+  return appScores
+    .filter((app) => app.score > 0)
+    .sort((a, b) => b.score - a.score)
+    .slice(0, limit)
+}
+
+export { findSimilarApps, type SimilarApp }
