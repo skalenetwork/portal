84b476c6fe40b85807527d4d3ed903687d12f1e9
Dmytro <dmitry.tkk@gmail.com>
2025-09-10 17:02:25 +0100

    Add dynamic deps build in dev mode, update build script

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 import mdx from "@mdx-js/rollup"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mdx()]
-  // assetsInclude: ["**/*.mdx"]
+  plugins: [react(), mdx()],
+  resolve: {
+    alias: {
+      '@skalenetwork/metaport': path.resolve(__dirname, 'packages/metaport/src'),
+      '@skalenetwork/metaport/dist/style.css': path.resolve(__dirname, 'packages/metaport/src/dist/style.css'),
+      '@/core': path.resolve(__dirname, 'packages/core/src')
+    }
+  }
 })
