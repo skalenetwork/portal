40a4af60bf1a0df5a3a449d002e304113241288a
Dmytro <dmitry.tkk@gmail.com>
2025-09-10 18:19:58 +0100

    Debug metaport build script

--- packages/metaport/build.sh
+++ packages/metaport/build.sh
@@ -27,6 +27,12 @@ echo "Generating imports in ${META_DIR}..."
 node $SCRIPTS_DIR/generate-imports.cjs $META_DIR
 echo "Imports generated."
 
+
+echo '--------------'
+cat $META_DIR_EXTERNAL/mainnet/chains.json
+echo '--------------'
+
+
 echo '--------------'
 cat $META_DIR/mainnet/icons/index.ts
 echo '--------------'
