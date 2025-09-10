d47229ee1fcc4b692093a838cac80495b43a4dd0
Dmytro <dmitry.tkk@gmail.com>
2025-09-10 19:04:57 +0100

    Add more debug info in metaport build

--- packages/metaport/build.sh
+++ packages/metaport/build.sh
@@ -1,38 +1,37 @@
 #!/usr/bin/env bash
 
-set -e 
+set -euo pipefail
 
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export PORTAL_DIR="$DIR/../.."
-export SCRIPTS_DIR="$PORTAL_DIR/scripts"
+export PORTAL_DIR="${DIR}/../.."
+export SCRIPTS_DIR="${PORTAL_DIR}/scripts"
 
-META_DIR_EXTERNAL=$PORTAL_DIR/skale-network/metadata/
-META_DIR=$DIR/src/meta
+META_DIR_EXTERNAL="${PORTAL_DIR}/skale-network/metadata"
+META_DIR="${DIR}/src/meta"
 
-if [ -d "$META_DIR" ]; then
+if [ -d "${META_DIR}" ]; then
     echo "Removing ${META_DIR}..."
-    rm -rf $META_DIR
+    rm -rf "${META_DIR}"
 else
     echo "${META_DIR} not found, skipping"
 fi
 
-mkdir -p $META_DIR
+mkdir -p "${META_DIR}"
 
 echo "Copying ${META_DIR_EXTERNAL} -> ${META_DIR}..."
-cp -R $META_DIR_EXTERNAL $META_DIR/
+cp -R "${META_DIR_EXTERNAL}/"* "${META_DIR}"/
 
-cp -R $PORTAL_DIR/config/testnet $DIR/src/meta/testnetConfig
+cp -R "${PORTAL_DIR}/config/testnet" "${META_DIR}/testnetConfig"
 
 echo "Generating imports in ${META_DIR}..."
-node $SCRIPTS_DIR/generate-imports.cjs $META_DIR
+node "${SCRIPTS_DIR}/generate-imports.cjs" "${META_DIR}"
 echo "Imports generated."
 
 echo '--------------'
-cat $META_DIR_EXTERNAL/mainnet/chains.json
+cat "${META_DIR_EXTERNAL}/mainnet/chains.json"
 echo '--------------'
 
-
 echo '--------------'
-cat $META_DIR/mainnet/chains.json
-cat $META_DIR/mainnet/icons/index.ts
+cat "${META_DIR}/mainnet/chains.json"
+cat "${META_DIR}/mainnet/icons/index.ts"
 echo '--------------'
