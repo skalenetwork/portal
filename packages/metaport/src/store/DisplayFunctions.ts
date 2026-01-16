84b07f79d7ed34e0234197b619739d7c8ac0c6c2
ines-SKALE <ines@skalelabs.com>
2026-01-16 20:22:47 +0000

    comment out sFuelOk

--- packages/metaport/src/store/DisplayFunctions.ts
+++ packages/metaport/src/store/DisplayFunctions.ts
@@ -24,7 +24,10 @@
 import { type types, constants } from '@/core'
 import { useCollapseStore } from '../store/Store'
 import { useMetaportStore } from '../store/MetaportStore'
-import { useSFuelStore } from '../store/SFuelStore'
+
+// todo: tmp fix: disable sFuel check for stepper display
+
+// import { useSFuelStore } from '../store/SFuelStore'
 
 export type DisplayFunctions = {
   showFrom: () => boolean
@@ -49,15 +52,17 @@ export const useDisplayFunctions = (): DisplayFunctions => {
   const token = useMetaportStore((state) => state.token)
   const errorMessage = useMetaportStore((state) => state.errorMessage)
 
-  const fromChainData = useSFuelStore((state) => state.fromChainData)
-  const toChainData = useSFuelStore((state) => state.toChainData)
-  const hubChainData = useSFuelStore((state) => state.hubChainData)
+  // todo: tmp fix: disable sFuel check for stepper display
 
-  const fromOk = fromChainData && fromChainData.ok
-  const toOk = toChainData && toChainData.ok
-  const hubOk = (hubChainData && hubChainData.ok) || !hubChainData
+  // const fromChainData = useSFuelStore((state) => state.fromChainData)
+  // const toChainData = useSFuelStore((state) => state.toChainData)
+  // const hubChainData = useSFuelStore((state) => state.hubChainData)
 
-  const sFuelOk = fromOk && toOk && hubOk
+  // const fromOk = fromChainData && fromChainData.ok
+  // const toOk = toChainData && toChainData.ok
+  // const hubOk = (hubChainData && hubChainData.ok) || !hubChainData
+
+  // const sFuelOk = fromOk && toOk && hubOk      
 
   const showFrom = (): boolean => {
     return !expandedTo && !expandedTokens && !errorMessage && !expandedCP && !expandedTH
