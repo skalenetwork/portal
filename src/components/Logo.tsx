7674c1b80844a57ecce30de0559abe35c61e55f3
ines-SKALE <ines@skalelabs.com>
2026-01-19 19:09:26 +0000

    dapp logo small instead of extra small

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -35,7 +35,7 @@ interface LogoProps {
 }
 
 const Logo: React.FC<LogoProps> = ({ chainsMeta, skaleNetwork, chainName, appName, size }) => {
-  size = size || 'xs'
+  size = size || 'sm'
   const { mode } = useThemeMode()
   return (
     <div className={cls('sk-app-logo', `sk-logo-${size}`, 'br__tile')}>
