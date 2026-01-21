8ab8948a253a071b3e2bf07f4ec00ff1ce543f84
ines-SKALE <ines@skalelabs.com>
2026-01-21 16:37:14 +0000

    wrong spelling corrected

--- src/components/ecosystem/SubcategoryList.tsx
+++ src/components/ecosystem/SubcategoryList.tsx
@@ -66,7 +66,7 @@ const SubcategoryList: React.FC<SubcategoryListProps> = ({
               />
             }
             label={
-              <span className= "text-xs text-xs00 ${mode === 'dark' ? 'text-white' : 'text-forerground'">
+              <span className= "text-xs text-xs00 ${mode === 'dark' ? 'text-white' : 'text-foreground'">
                 {highlightMatch(subcategory.name, searchTerm)}
               </span>
             }
