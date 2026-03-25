e1c2fb557f3ccb85cbfb82d46430f5682b85b297
ines-SKALE <ines@skalelabs.com>
2026-03-25 12:52:12 +0000

    added amount for SKL Staked Successfully after merge conflicts

--- src/components/delegation/Delegate.tsx
+++ src/components/delegation/Delegate.tsx
@@ -114,7 +114,7 @@ export default function Delegate(props: {
         'delegation:delegate'
       )
       setLoading(false)
-      notify.temporarySuccess('SKL staked successfully')
+      notify.temporarySuccess(`${amount} SKL staked successfully`)
       navigate('/staking')
     } catch (err: any) {
       log.error(err)
