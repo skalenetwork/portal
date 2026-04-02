9edbf41a07afc8186e9f1ad760d16bad6f7cb5bd
ines-SKALE <ines@skalelabs.com>
2026-04-02 15:50:45 +0100

    added blur bg on the dark mode

--- packages/core/src/notify.ts
+++ packages/core/src/notify.ts
@@ -26,19 +26,19 @@ import { toast } from 'sonner'
 const DEFAULT_DURATION = 5000
 
 function temporarySuccess(message: string, toastId?: string | number) {
-    toast.success(message, { duration: DEFAULT_DURATION, id: toastId, className: "text-emerald-500! dark:text-emerald-400! bg-emerald-100! dark:bg-emerald-700!" })
+    toast.success(message, { duration: DEFAULT_DURATION, id: toastId, className: "text-emerald-500! dark:text-emerald-400! bg-emerald-100! dark:bg-emerald-400/15! backdrop-blur-md!" })
 }
 
 function temporaryInfo(message: string, toastId?: string | number) {
-    toast.info(message, { duration: DEFAULT_DURATION, id: toastId, className: "text-sky-500! dark:text-blue-400! bg-sky-100! dark:bg-blue-700!" })
+    toast.info(message, { duration: DEFAULT_DURATION, id: toastId, className: "text-sky-500! dark:text-blue-400! bg-sky-100! dark:bg-blue-400/15! backdrop-blur-md!" })
 }
 
 function permanentError(message: string, toastId?: string | number) {
-    toast.error(message, { duration: Infinity, id: toastId, className: "text-red-500! dark:text-red-400! bg-red-100! dark:bg-red-700!" })
+    toast.error(message, { duration: Infinity, id: toastId, className: "text-red-500! dark:text-red-400! bg-red-100! dark:bg-red-400/15! backdrop-blur-md!" })
 }
 
 function temporaryError(message: string, toastId?: string | number) {
-    toast.error(message, { duration: DEFAULT_DURATION, id: toastId, className: "text-red-500! dark:text-red-400! bg-red-100! dark:bg-red-700!" })
+    toast.error(message, { duration: DEFAULT_DURATION, id: toastId, className: "text-red-500! dark:text-red-400! bg-red-100! dark:bg-red-400/15! backdrop-blur-md!" })
 }
 
 function loading(message: string, options?: any): string | number {
